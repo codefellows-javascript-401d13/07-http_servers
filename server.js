47b496549cc4828fc40a7436c2847c3eca810939
GLSea1979 <builtbygl@gmail.com>
2017-02-21 13:44:08 -0800

    initial build

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const parseBody = require('./lib/parse-body.js');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+  console.log('request url:', req.url);
+});
+
+
+server.listen(PORT, function() {
+  console.log('server up:', PORT);
+})
