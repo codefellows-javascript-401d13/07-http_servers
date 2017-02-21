2502200397586ca8ac80378dc18b3247e1d250fd
Dana Kulp <dkulp23@gmail.com>
2017-02-21 13:30:06 -0800

    Required in http create server method and created instance. Tested in server and console logged results.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const http = require('http').createServer;
+const PORT = process.env.PORT || 3000;
+
+const server = http(function(req, res) {
+  if (req) console.log(`req: ${req}`);
+  if (res) console.log(`res: ${res}`);
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log(`Server's up dude! PORT:${PORT}`);
+});
