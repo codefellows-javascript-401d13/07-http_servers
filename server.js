7da9f83fb354620b52bc393f7074947230f24e28
brials <brials008@hotmail.com>
2017-02-21 14:45:16 -0800

    setup some initial stuff

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const cowsay = require('cowsay');
+const PORT = process.env.PORT || 3000;
+const parseBody = require('./lib/parse-body.js');
+const server = http.createServer(function(req, res){
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+
+});
+
+server.listen(PORT, function(){
+  console.log(PORT);
+});
