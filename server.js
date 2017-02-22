be87c612801071749e8014533bab2e03c0b4023d
GLSea1979 <builtbygl@gmail.com>
2017-02-21 20:30:47 -0800

    why can't I query?

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
-  req.url.query.text = querystring.parse(req.url.query.text);
+  req.url.query.text = querystring.parse(req.url.query);
 
   // console.log('req body:', req.url);
   console.log('req query', req.url.query);
@@ -25,27 +25,20 @@ const server = http.createServer(function(req, res) {
     res.write('Hello from my server');
     res.end();
   }
-  if (req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query === 'text') {
+  if (req.method === 'GET' && req.url.pathname === '/cowsay' && req.url.query.text) {
     res.writeHead(200, {
-      'Content-Type': 'text/plain'
+      'Content-Type': 'text/plain'u
     });
-    console.log('inside the extension');
+    res.write(cowsay({f: 'tux', text: `${req.url.query.text}`}))
   }
   if (req.method === 'GET' && req.url.pathname === '/cowsay') {
-    res.writeHead(200, {
+    res.writeHead(400, {
       'Content-Type': 'text/plain'
     });
-    res.write(cowsay.say({ text: 'hi there!'}));
+    res.write(cowsay.say({f: 'dragon', text: 'hi there, that was a bad request. Input some text next time human!'}));
     res.end();
   };
 
-  // if (req.method === 'GET' && req.url.pathname === '/fortune') {
-  //   res.writeHead(200, {
-  //     'Content-Type': 'text/plain'
-  //   });
-  //   res.write(fortune.say);
-  //   res.end();
-  // }
 
   if (req.method === 'POST') {
     parseBody(req, function(err) {
