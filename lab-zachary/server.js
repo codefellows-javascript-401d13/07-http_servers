74f266d346a3b39b5d73ec4d0d402c859146798a
Zachary Crumbo <zcrumbo@mac.com>
2017-02-21 20:29:57 -0800

    Cleaned up server.js to pass linting

--- lab-zachary/server.js
+++ lab-zachary/server.js
@@ -10,24 +10,24 @@ const server = http.createServer(function(req, res){
   var pathname = url.parse(req.url).pathname;
   var query = url.parse(req.url, true).query;
   if (pathname == '/'){
-      res.statusCode = 200;
-      res.setHeader('Content-type', 'text/plain');
-      res.write('Hello from my server!');
-      res.end()
-      return;
-  };
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.write('Hello from my server!');
+    res.end();
+    return;
+  }
   if (req.method === 'GET' && pathname === '/cowsay'){
     res.setHeader('Content-type', 'text/plain');
     if(typeof(query.text)==='undefined'){
       res.statusCode = 400;
       res.write(cowsay.say({text: 'You made a bad request. Shame.', f: 'default', e : 'xO', T : 'U'}));
-      res.end()
+      res.end();
       return;
-    };
+    }
     res.write(cowsay.say({text: query.text, f: (query.cow ? query.cow :'default') }));
     res.end();
     return;
-  };
+  }
   if(req.method === 'POST' && pathname === '/cowsay') {
     parseBody(req, function(err, data){
       if(err) {
@@ -45,14 +45,14 @@ const server = http.createServer(function(req, res){
       res.end();
       return;
     });
-  };
+  }
 
   //unhandled
   if (pathname !== '/' && pathname !== '/cowsay'){
     res.statusCode = 501;
     res.write(cowsay.say({text: 'This route hasn\'t been set up yet. Shame.', f: 'cheese'}));
     res.end();
-  };
+  }
 
 });
 
