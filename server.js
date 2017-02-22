242247b8be3b9ededab365477a0682f6dffc4bb3
Dana Kulp <dkulp23@gmail.com>
2017-02-21 20:38:10 -0800

    Walked away for a bit to clear my head. Realized what my brain block was...I think.

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 'use strict';
 
-const http = require('http').createServer;
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const bodyParser = require(`${__dirname}/lib/body-parser.js`);
+const cowsay = require('cowsay');
 const PORT = process.env.PORT || 3000;
 
-const server = http(function(req, res) {
-  if (req) console.log(`req: ${req}`);
-  if (res) console.log(`res: ${res}`);
+const server = http.createServer(function(req, res) {
+  req.url = url.parse(req.url);
+  req.url.query = querystring.parse(req.url.query);
+  // if (req.method === 'POST' && req.url.pathname === '/cowsay') {
+  //   //TODO: Handle POST requests made to cowsay using body parser
+  // }
+
+  if(req.method === 'GET' && req.url.pathname === '/cowsay') {
+    if (req.url.query) console.log('object passed conditional');
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.write(cowsay.say({ text: 'hello' }));
+    res.end();
+  }
+
+  if (req.method === 'GET' && req.url.pathname === '/') {
+    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain'});
+    res.write('Hello from my server!');
+    res.end();
+  }
+
+  if (req.method === 'POST' && req.url.pathname === '/') {
+    res.writeHead(200, 'OK', { 'Content-Type': 'text/plain'});
+    res.write('Hello from my server!');
+    res.end();
+  }
   res.end();
 });
 
