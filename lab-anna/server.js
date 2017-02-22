fd955537d985b1c5f1e0027e25a07fa0fdfcd612
Anna Ulanova <anetteua0@gmail.com>
2017-02-21 23:21:15 -0800

    MVP working

--- lab-anna/server.js
+++ lab-anna/server.js
@@ -7,17 +7,30 @@ const cowsay = require('cowsay');
 const parseBody = require('./lib/parse-body.js');
 const PORT = process.env.PORT || 3000;
 
+const cowSays = function(obj, res) {
+  if (obj === undefined) {
+    res.writeHead(400, {'Content-Type': 'text/plain'});
+    res.write(cowsay.say({ text: 'bad request' }));
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(cowsay.say({ text: obj}));
+  };
+};
+
 const server = http.createServer(function(req, res) {
   req.url = url.parse(req.url);
   req.url.query = querystring.parse(req.url.query);
 
-
   if (req.method === 'POST' && req.url.pathname === '/cowsay') {
     parseBody(req, function(err, parsed) {
       console.log("HH", err, req.body, parsed, req.body.text);
-      if (err) return console.error(err);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(cowsay.say({ text: req.body.text}));
+      if (err) {
+        cowSays(undefined, res);
+        res.end();
+        return;
+      };
+
+      cowSays(req.body.text, res);
       res.end();
     });
     return;
@@ -30,14 +43,7 @@ const server = http.createServer(function(req, res) {
     };
 
     if (req.url.pathname === '/cowsay') {
-
-      if (req.url.query.text === undefined) {
-        res.writeHead(400, {'Content-Type': 'text/plain'});
-        res.write(cowsay.say({ text: 'bad request' }));
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(cowsay.say({ text: req.url.query.text }));
-      };
+      cowSays(req.url.query.text, res);
     };
     res.end();
   };
