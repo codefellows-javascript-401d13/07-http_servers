4ad1077470bc3f3b6f9c42f49334de97da8e4391
darms <darmcca@gmail.com>
2017-03-05 22:01:38 -0800

    basic server created

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const cowsay = require('cowsay');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer function(req, res) {
+  //TODO: Stop here and return
+});
